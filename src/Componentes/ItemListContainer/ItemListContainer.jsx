9e17744aca77b36db3e4eb6289e3f4718d26747f
FedeGreni <fedegreni>
2024-10-02 08:40:40 -0300

    Que sea esta viejo

--- src/Componentes/ItemListContainer/ItemListContainer.jsx
+++ src/Componentes/ItemListContainer/ItemListContainer.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import ItemList from "../ItemList/ItemList";
 import Loader from "../Loader/Loader";
 import { useParams } from "react-router-dom";
-import "./ItemListContainer.css";
 import { db } from "../../services/config";
 import { getDocs, collection, query, where } from "firebase/firestore";
 import './ItemListContainer.css';
